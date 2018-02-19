584c0a5df09cd62ce265367f842c09980ee0da64
palkiakerr <mxwkerr@gmail.com>
2018-02-19 10:44:34 +0000

    Update archinstall.sh
    Now running parted commands individuallt

--- archscript/archinstall.sh
+++ archscript/archinstall.sh
@@ -14,17 +14,15 @@
 
 #parted commands follow
 #Put this in a text file and do parted < fillename
-command < echo <<EndOfMessage
-mklabel gpt
-unit mib
-mkpart primary 1 3
-name 1 grub
-set 1 bios_grub on
-mkpart primary 3 131
-name 2 boot
-mkpart primary 131 643
-name 3 swap
-mkpart primary 643 -1
-name 4 rootfs
-set 2 boot on
-EndOfMessage
+parted < mklabel gpt
+parted < unit mib
+parted < mkpart primary 1 3
+parted < name 1 grub
+parted < set 1 bios_grub on
+parted < mkpart primary 3 131
+parted < name 2 boot
+parted < mkpart primary 131 643
+parted < name 3 swap
+parted < mkpart primary 643 -1
+parted < name 4 rootfs
+parted < set 2 boot on
