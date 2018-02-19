a6932e7580aef05e3fd9c1551094f0ad20500505
palkiakerr <mxwkerr@gmail.com>
2018-02-19 10:55:50 +0000

    Moved parted commands to a separate text file

--- archscript/archinstall.sh
+++ archscript/archinstall.sh
@@ -11,18 +11,8 @@
 # Partition Disks
 # Number should be adjusted before script runs (later passed as args)
 
-
+# Speeds up download of git
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+pacman -S git
 #parted commands follow
 #Put this in a text file and do parted < fillename
-parted < mklabel gpt
-parted < unit mib
-parted < mkpart primary 1 3
-parted < name 1 grub
-parted < set 1 bios_grub on
-parted < mkpart primary 3 131
-parted < name 2 boot
-parted < mkpart primary 131 643
-parted < name 3 swap
-parted < mkpart primary 643 -1
-parted < name 4 rootfs
-parted < set 2 boot on
