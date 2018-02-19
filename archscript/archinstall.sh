8bd8c535b96ef8479eb47545d2775884ad6fd0fa
palkiakerr <mxwkerr@gmail.com>
2018-02-19 11:03:00 +0000

    Update archinstall.sh

--- archscript/archinstall.sh
+++ archscript/archinstall.sh
@@ -15,5 +15,6 @@
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+pacman -Sy
 pacman -S git
 parted < partcommands
